c657a96661c5cf22e990c7422c408645f6a3afbd
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-12-23 22:10:57 +0000

    Fix friends lol

--- hooks/friends.js
+++ hooks/friends.js
@@ -2,13 +2,19 @@ import client from '../lib/supabase'
 import useSWR from 'swr'
 
 export function useFriends() {
+  const { id } = client.auth.user()
   return useSWR('friends', async (key) => {
-    const { data, ...response } = await client.from(key).select(`
+    const { data } = await client
+      .from(key)
+      .select(
+        `
       friend:friend_id (
         id,
         username
       )
-    `)
+    `
+      )
+      .eq('user_id', id)
 
     return data
   })
